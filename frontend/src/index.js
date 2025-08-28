2b7d532ca658f899a42c5dcedc35b3e435223afd
GodGomzi <gou250904@gmail.com>
2025-08-28 23:36:24 +0530

    Index.js and .css added

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // You can create this file for global styles if you wish
+import App from './App';
+
+// This line finds the <div id="root"> from your public/index.html file.
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+// This line tells React to render your entire <App> component inside that div.
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
